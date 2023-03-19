45f933c04ecd7786a3658231eef82a49331432ca
Denis Plotnikov <denis.plotnikov.82@ya.com>
2023-03-19 20:15:08 +0500

    new info is added

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿string ReadData(string msg)
+ 
+{
+    Console.Write(msg);
+    return (Console.ReadLine() ?? "0");
+}
+ 
+string arr = ReadData("Введите слова через пробел: ");
+ 
+string[] array1 = arr.Split(" ".ToArray(), StringSplitOptions.RemoveEmptyEntries).ToArray();
+string[] array2 = new string[array1.Length];
+ 
+int FilterString(string[] array1, string[] array2)
+{
+ 
+    int j = 0;
+ 
+    for (int i = 0; i < array1.Length; i++)
+ 
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[j] = array1[i];
+            j++;
+        }
+ 
+    }
+    return j;
+}
+ 
+void Print1DArr(string[] array)
+{
+ 
+    Console.Write("['");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + "', '");
+    }
+    Console.WriteLine(array[array.Length - 1] + "']");
+ 
+}
+ 
+Print1DArr(array1);
+FilterString(array1, array2);
+int j = FilterString(array1, array2);
+Array.Resize(ref array2, j);
+Print1DArr(array2);
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# FTEST
+# Flowchart: 
+C:\Users\fordm\OneDrive\Рабочий стол\Обучение GB\C#\FTEST\Checkwork_Dev.jpg
+
